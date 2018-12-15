f5099e638587c872065627106fbbd043a9e43389
lico liu <licoliu@licos-MacBook-Pro.local>
2018-12-15 14:13:30 +0800

    add image property on notice.

--- packages/taro-material/src/Notice/index.js
+++ packages/taro-material/src/Notice/index.js
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { View, Text } from '@tarojs/components';
+import { View, Text, Image } from '@tarojs/components';
 
 import AtComponent from '../common/component';
 
@@ -16,11 +16,13 @@ class Notice extends AtComponent {
   };
 
   render() {
-    const { title, content, rows, color } = this.props;
+    const { title, content, rows, color, icon } = this.props;
     const { date } = this.state;
+
+    const height = (rows * 36) / 2;
+
     let style = {};
     if (rows) {
-      const height = (rows * 36) / 2;
       style = {
         maxHeight: `${height}px`,
         minHeight: `${height}px`,
@@ -39,12 +41,24 @@ class Notice extends AtComponent {
       [`color${_color}`]: color !== 'inherit',
     };
 
+    const iconStyle = {
+      width: `${height}px`,
+      height: `${height}px`,
+    };
+
     return (
-      <View className={classNames(classObject, this.props.className)} style={style}>
-        {title && <View className="subheading">{title}</View>}
-        {content && <View className="body2">{content}</View>}
-        <View className="body2">{this.props.children}</View>
-        <View className="caption">{ago(date)}</View>
+      <View className={classNames(classObject, this.props.className)}>
+        <View className="rm-notice-content" style={style}>
+          {title && <View className="subheading">{title}</View>}
+          {content && <View className="body2">{content}</View>}
+          <View className="body2">{this.props.children}</View>
+          <View className="caption">{ago(date)}</View>
+        </View>
+        {icon && (
+          <View className="rm-notice-icon" style={iconStyle}>
+            <Image style={{ width: '100%', height: '100%' }} mode="aspectFill" src={icon} />
+          </View>
+        )}
       </View>
     );
   }
@@ -54,12 +68,26 @@ Notice.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
   rows: PropTypes.number,
+  color: PropTypes.oneOf([
+    'inherit',
+    'primary',
+    'secondary',
+    'action',
+    'error',
+    'disabled',
+    'success',
+    'warning',
+    'progress',
+    'default',
+  ]),
+  icon: PropTypes.string,
 };
 Notice.defaultProps = {
   title: '',
   content: '',
   rows: 0,
   color: 'default',
+  icon: '',
 };
 
 export default Notice;
