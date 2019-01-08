bdb233e86f090d21d9aad848f7c6c5e3728dbe96
lico liu <licoliu@licos-MacBook-Pro.local>
2019-01-08 21:30:30 +0800

    bug fixed.

--- packages/taro-material/src/ToolBar/index.js
+++ packages/taro-material/src/ToolBar/index.js
@@ -13,7 +13,14 @@ import theme from '../styles/theme';
 import './ToolBar.scss';
 
 const cloneDeep = o => JSON.parse(JSON.stringify(o));
-const isEqual = (array1 = [], array2 = []) => {
+const isEqual = (array1, array2) => {
+  if (array1 === array2) {
+    return true;
+  }
+  if (!(array1 && array2)) {
+    return false;
+  }
+
   if (array1.length !== array2.length) {
     return false;
   }
