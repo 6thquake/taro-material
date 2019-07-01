51a44edf93f18299d54875fedbca1ad893796734
lico liu <licoliu@licos-MacBook-Pro.local>
2019-07-01 15:17:17 +0800

    feat(float bar): 增加是否显示阴影的配置项raised

--- packages/taro-material/src/FloatBar/index.js
+++ packages/taro-material/src/FloatBar/index.js
@@ -3,67 +3,72 @@ import { View } from '@tarojs/components';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import theme from '../styles/theme.js';
-import {mergeStyle} from '../utils/styles.js';
+import { mergeStyle } from '../utils/styles.js';
 import './FloatBar.scss';
 
 class RMFloatBar extends Component {
-
   render() {
-    const {placement,offset,color,spacing,fixed,autoWidth,customStyle} = this.props;
-    const fixedClass = classNames(
-      'at-float-bar',
-      {
-        // 'at-float-bar--fixed': fixed,
-        [`at-float-bar--fixed-${placement}`]:fixed,
-      },
-    );
-    const childClass = classNames(
-      'at-float-bar-child',
-    )
-    let colorStyle= `background-color: ${theme.palette[`${color}`]['main']};color:${theme.palette[`${color}`]['contrastText']};${customStyle};`;
+    const { placement, offset, color, spacing, fixed, autoWidth, customStyle, raised } = this.props;
+    const fixedClass = classNames('at-float-bar', {
+      // 'at-float-bar--fixed': fixed,
+      [`at-float-bar--fixed-${placement}`]: fixed,
+      raised,
+    });
+    const childClass = classNames('at-float-bar-child');
+    let colorStyle = `background-color: ${theme.palette[`${color}`]['main']};color:${
+      theme.palette[`${color}`]['contrastText']
+    };${customStyle};`;
 
     let offsetStyle;
-    if(placement=='top'){
-        offsetStyle= `top:${offset}px;`
-    }else{
-        offsetStyle= `bottom:${offset}px;`
+    if (placement == 'top') {
+      offsetStyle = `top:${offset}px;`;
+    } else {
+      offsetStyle = `bottom:${offset}px;`;
     }
     let spacStyle;
-    if(!autoWidth){
-      spacStyle=`position:absolute;left:${spacing}px;right:${spacing}px;`
-    }else{
-      spacStyle=``;
+    if (!autoWidth) {
+      spacStyle = `position:absolute;left:${spacing}px;right:${spacing}px;`;
+    } else {
+      spacStyle = ``;
     }
 
     return (
-        <View className={fixedClass} style={mergeStyle(offsetStyle)}>
-          <View className={childClass} style={mergeStyle(colorStyle,spacStyle)}>
-            {this.props.children}
-          </View>
+      <View className={fixedClass} style={mergeStyle(offsetStyle)}>
+        <View className={childClass} style={mergeStyle(colorStyle, spacStyle)}>
+          {this.props.children}
+        </View>
       </View>
     );
   }
 }
 
 RMFloatBar.defaultProps = {
-    customStyle:'',
-    placement:'bottom',
-    offset:60,
-    color:'default',
-    spacing:8,
-    fixed: false,
+  customStyle: '',
+  placement: 'bottom',
+  offset: 60,
+  color: 'default',
+  spacing: 8,
+  fixed: false,
+  raised: false,
 };
 
 RMFloatBar.propTypes = {
-    customStyle: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object
-    ]),
-    placement:PropTypes.oneOf(['top', 'bottom']),
-    offset:PropTypes.number,
-    color:PropTypes.oneOf(['default','inherit' , 'primary' , 'secondary' ,'error' , 'success' , 'warning' , 'progress']),
-    spacing:PropTypes.number,
-    fixed: PropTypes.bool,
+  customStyle: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  placement: PropTypes.oneOf(['top', 'bottom']),
+  offset: PropTypes.number,
+  color: PropTypes.oneOf([
+    'default',
+    'inherit',
+    'primary',
+    'secondary',
+    'error',
+    'success',
+    'warning',
+    'progress',
+  ]),
+  spacing: PropTypes.number,
+  fixed: PropTypes.bool,
+  raised: PropTypes.bool,
 };
 
 export default RMFloatBar;
--- pages/pages/material/float-bar/index.js
+++ pages/pages/material/float-bar/index.js
@@ -1,14 +1,13 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View } from '@tarojs/components';
 
-import { RMFloatBar,RMIcon } from '../../../';
+import { RMFloatBar } from '../../../';
 import './index.scss';
 
 export default class FloatBarPage extends Component {
   config = {
     navigationBarTitleText: 'FloatBar',
   };
-
 
   render() {
     return (
@@ -27,9 +26,7 @@ export default class FloatBarPage extends Component {
                   // fixed
                   // color={'grey'}
                 >
-                  <View>
-                    不固定，宽度百分百
-                  </View>
+                  <View>不固定，宽度百分百</View>
                 </RMFloatBar>
                 <RMFloatBar
                   placement={'top'}
@@ -39,9 +36,7 @@ export default class FloatBarPage extends Component {
                   autoWidth
                   // color={'grey'}
                 >
-                  <View>
-                    固定顶部,宽度自适应
-                  </View>
+                  <View>固定顶部，宽度自适应</View>
                 </RMFloatBar>
               </View>
             </View>
@@ -62,33 +57,49 @@ export default class FloatBarPage extends Component {
                   fixed
                   color={'success'}
                 >
-                  <View>
-                    固定底部,宽度自适应
-                  </View>
+                  <View>固定底部，宽度自适应</View>
                 </RMFloatBar>
               </View>
             </View>
           </View>
         </View>
 
+        <View className="doc-body">
+          {/* 基本用法 */}
+          <View className="panel">
+            <View className="panel__title">基本用法</View>
+            <View className="panel__content no-padding">
+              <View className="example-item">
+                <RMFloatBar
+                  placement={'bottom'}
+                  offset={180}
+                  spacing={10}
+                  autoWidth
+                  fixed
+                  raised
+                  color={'primary'}
+                >
+                  <View>固定底部，宽度自适应，加阴影</View>
+                </RMFloatBar>
+              </View>
+            </View>
+          </View>
+        </View>
 
         <View className="doc-body">
           {/* 基本用法 */}
           <View className="panel">
             <View className="panel__title">参数说明</View>
             <View className="panel__content no-padding">
+              <View className="example-item" />
               <View className="example-item">
-
-              </View>
-              <View className="example-item">
-                autoWidth:是否自适应宽度。如果值为false,那么该组件的宽度默认是100%。Boolean值。有autoWidth的时候spaing:表示的是距离两端的间隙的值 
+                autoWidth:是否自适应宽度。如果值为false,那么该组件的宽度默认是100%。Boolean值。有autoWidth的时候spaing:表示的是距离两端的间隙的值
               </View>
               <View className="example-item">
                 spacing:当autoWidth值为false是发挥作用。值是一个数字(number)。
               </View>
-              <View className="example-item">
-                fixed：是否是固定定位。默认为false。Boolean值
-              </View>
+              <View className="example-item">fixed：是否是固定定位。默认为false。Boolean值</View>
+              <View className="example-item">raised：是否显示阴影。默认为false。Boolean值</View>
               <View className="example-item">
                 placement:值是一个String类型的值（'top'|'bottom'）,表示布局的时候是顶部或者是底部
               </View>
@@ -96,7 +107,8 @@ export default class FloatBarPage extends Component {
                 offset:值是number类型的值，与placement一起使用。比如placement为top:offset的值表示距离顶部偏移的值。
               </View>
               <View className="example-item">
-                color:组件的背景色。String可取的值：'default' | 'inherit' | 'primary' | 'secondary' | 'error' | 'success' | 'warning' | 'progress'
+                color:组件的背景色。String可取的值：'default' | 'inherit' | 'primary' | 'secondary'
+                | 'error' | 'success' | 'warning' | 'progress'
               </View>
             </View>
           </View>
